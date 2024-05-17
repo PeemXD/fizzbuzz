3d11fec3753ce776ecc06b47bb0077f30dadb419
Peem Auppati <peem.a@arise.tech>
2024-05-17 11:51:05 +0700

    should return Buzz when given 5

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -6,6 +6,9 @@ func fizzBuzz(num int) string {
 	if num == 3 {
 		return "Fizz"
 	}
+	if num == 5 {
+		return "Buzz"
+	}
 
 	return fmt.Sprintf("%v", num)
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -47,4 +47,15 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("want %v but got %v\n", want, got)
 		}
 	})
+
+	t.Run("should return Buzz when given 5", func(t *testing.T) {
+		num := 5
+		want := "Buzz"
+
+		got := fizzBuzz(num)
+
+		if want != got {
+			t.Errorf("want %v but got %v\n", want, got)
+		}
+	})
 }
