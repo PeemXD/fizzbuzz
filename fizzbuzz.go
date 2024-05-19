c3afa6617e2be1d97dcf481fcb900e56d4432c58
Peem Auppati <peem.a@arise.tech>
2024-05-19 10:04:22 +0700

    should return Fizz when given 3

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func fizzBuzz(i int) string {
+	if i%3 == 0 {
+		return "Fizz"
+	}
 	return fmt.Sprintf("%v", i)
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -23,6 +23,10 @@ func TestFizzBuzz(t *testing.T) {
 			given: 2,
 			want:  "2",
 		},
+		{
+			given: 3,
+			want:  "Fizz",
+		},
 	}
 
 	for _, tt := range tests {
