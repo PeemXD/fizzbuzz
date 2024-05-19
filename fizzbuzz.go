51a18f0f1950d0b773c46a7c40e94e600699d233
Peem Auppati <peem.a@arise.tech>
2024-05-19 15:19:31 +0700

    should return 1 when given 1

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,5 +1,8 @@
 package main
 
-func fizzBuzz() {
+import "strconv"
+
+func fizzBuzz(i int) string {
+	return strconv.Itoa(1)
 
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -3,5 +3,14 @@ package main
 import "testing"
 
 func TestFizzBuzz(t *testing.T) {
+	t.Run("should return 1 when given 1", func(t *testing.T) {
+		give := 1
+		want := "1"
 
+		got := fizzBuzz(give)
+
+		if want != got {
+			t.Errorf("want %v but got %v", want, got)
+		}
+	})
 }
