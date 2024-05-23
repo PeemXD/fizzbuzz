0d38c888fbb1877b018a7a4879c708e71100b5b0
Peem Auppati <peem.a@arise.tech>
2024-05-23 15:53:21 +0700

    refactor test case

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -1,8 +1,56 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFizzBuzz(t *testing.T) {
+	tests := []struct {
+		name  string
+		given int
+		want  string
+	}{
+		{
+			name:  "should return 1 when given 1",
+			given: 1,
+			want:  "1",
+		},
+		{
+			name:  "should return 2 when given 2",
+			given: 2,
+			want:  "2",
+		},
+		{
+			name:  "should return Fizz when given 3",
+			given: 3,
+			want:  "Fizz",
+		},
+		{
+			name:  "should return 4 when given 4",
+			given: 4,
+			want:  "4",
+		},
+		{
+			name:  "should return Buzz when given 5",
+			given: 5,
+			want:  "Buzz",
+		},
+		{
+			name:  "should return Fizz when given 6",
+			given: 6,
+			want:  "Fizz",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := fizzBuzz(tt.given)
+
+			if tt.want != got {
+				t.Errorf("want %v but got %v", tt.want, got)
+			}
+		})
+	}
 	t.Run("should return 1 when given 1", func(t *testing.T) {
 		given := 1
 		want := "1"
