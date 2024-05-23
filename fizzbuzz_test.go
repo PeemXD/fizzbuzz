f1b2f4cabdedd820e1f97830ce355c9e041131fa
Peem Auppati <peem.a@arise.tech>
2024-05-23 15:41:32 +0700

    should return 4 when given 4

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -33,4 +33,14 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("want %v but got %v", want, got)
 		}
 	})
+	t.Run("should return Fizz when given 3", func(t *testing.T) {
+		given := 4
+		want := "4"
+
+		got := fizzBuzz(given)
+
+		if want != got {
+			t.Errorf("want %v but got %v", want, got)
+		}
+	})
 }
