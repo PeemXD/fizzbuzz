a90a96f405210f2ff92354d19aa4df4d8095232e
Peem Auppati <peem.a@arise.tech>
2024-05-30 10:46:35 +0700

    should return 1 when given 1

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,5 +1,23 @@
 package main
 
-func fizzBuzz() {
+import "strconv"
 
+type fbI interface {
+	isFizzBuzz(int) string
+}
+type fb struct {
+	s int
+}
+
+func newFb() fb {
+	return fb{s: 1}
+}
+
+func (f *fb) isFizzBuzz() string {
+	return strconv.Itoa(f.s)
+}
+
+func fizzBuzz(n int) string {
+	fb := newFb()
+	return fb.isFizzBuzz()
 }
